031b681e3695ed932f2eefaf7c24887730adde5d
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-10-27 19:17:16 +0100

    add gen_server test

--- websocket/websocket_gen_server_test.erl
+++ websocket/websocket_gen_server_test.erl
@@ -1,6 +1,21 @@
 -module({{name}}_gen_server_test).
 
+-import({{name}}_gen_server,[init/1]).
+
 -include_lib("eunit/include/eunit.hrl").
 
-{{name}}_test() ->
-    ?assert(true).
+{{name}}_test_() ->
+    {foreach,
+     fun setup/0,
+     fun cleanup/1,
+     [fun hello/0]}.
+
+hello() ->
+    Reply = gen_server:call(test, {hello, <<"hi">>}),
+    ?assertEqual(Reply, <<"That's what she said! hi">>).
+
+setup() ->
+    {ok, _} = gen_server:start({local, test}, {{name}}_gen_server, [], []).
+
+cleanup(_) ->
+    ok = gen_server:stop(test).
